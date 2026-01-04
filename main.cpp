12dfe675a881e6b5c7c8ba294e1e5af0df0893e0
Pere91 <peregm.91@gmail.com>
2026-01-04 18:51:57 +0100

    Reduce to sum up the number of elements in each bin

--- main.cpp
+++ main.cpp
@@ -12,7 +12,8 @@
 
 using namespace oneapi;
 
-int main() {
+int main()
+{
     std::vector<int> values = {0, 7, 8, 10, 24, 48, 73, 120};
     const int N = values.size();
     const int NUM_BINS = 3;
@@ -26,48 +27,68 @@ int main() {
 
     // Distribute the bins evenly
     auto bin_range_values = std::vector<int>(NUM_BINS);
-    for (int i = 0; i < NUM_BINS; i++) {
+    for (int i = 0; i < NUM_BINS; i++)
+    {
         bin_range_values[i] = MAX_VALUE - (NUM_BINS - 1 - i) * BIN_SPAN;
     }
- 
+
     // Map each value to its corresponding bin
-    std::vector<std::array<int, 3>> mapped_values(N);
+    std::vector<std::array<int, NUM_BINS>> mapped_values(N);
     oneapi::tbb::parallel_for(
         oneapi::tbb::blocked_range<int>(0, N),
-        [&](tbb::blocked_range<int> r){
-            for (int i = r.begin(); i < r.end(); i++) {
+        [&](tbb::blocked_range<int> r)
+        {
+            for (int i = r.begin(); i < r.end(); i++)
+            {
                 int val = values[i] > 0 ? values[i] - 1 : values[i];
                 int idx = val / BIN_SPAN;
-                std::array<int, 3> arr = {0, 0, 0};
+                std::array<int, NUM_BINS> arr{};
                 arr[idx]++;
                 mapped_values[i] = arr;
             }
-        }
-    );
+        });
 
-    for (int i = 0; i < mapped_values.size(); i++) {
-        std::cout << "{ ";
-        for (int x : mapped_values[i]) {
-            std:: cout << x << " "; 
-        }
+    // for (int i = 0; i < mapped_values.size(); i++)
+    // {
+    //     std::cout << "{ ";
+    //     for (int x : mapped_values[i])
+    //     {
+    //         std::cout << x << " ";
+    //     }
 
-        std::cout << "} ";
-    }
-
+    //     std::cout << "} ";
+    // }
 
     // Sum up all values for each bin
-    // auto bins = std::vector<int>(NUM_BINS);
-    // oneapi::tbb::parallel_reduce(
-    //     oneapi::tbb::blocked_range<int>(1, N),
-    //             {0, 0 ,0},
-    //             [&](tbb::blocked_range<int> r, int total){
-    //                 for (int i = r.begin(); i < r.end(); i++) {
-    //                     bins[mapped_values[i]]++;
-    //                 }
-    //             },
-    //             [&](int left, int right){
+    std::array<int, NUM_BINS> bins{};
+    bins = oneapi::tbb::parallel_reduce(
+        oneapi::tbb::blocked_range<int>(0, N),
+        std::array<int, NUM_BINS>{},
+        [&](oneapi::tbb::blocked_range<int> r, std::array<int, NUM_BINS> total)
+        {
+            for (int i = r.begin(); i < r.end(); i++)
+            {
+                for (int j = 0; j < NUM_BINS; j++)
+                {
+                    total[j] += mapped_values[i][j];
+                }
+            }
+            return total;
+        },
+        [&](std::array<int, NUM_BINS> left, std::array<int, NUM_BINS> right)
+        {
+            std::array<int, NUM_BINS> res{};
+            for (int i = 0; i < NUM_BINS; i++)
+            {
+                res[i] = left[i] + right[i];
+            }
+            return res;
+        });
 
-    //             }
-    // );
 
+    // for (int i : bins)
+    // {
+    //     std::cout << i << " ";
+    // }
+    // std::cout << std::endl;
 }
